83776ac5a0848a2ff4d057f4b9eba62be15410fb
Enes SVG <enessvg4@gmail.com>
2025-05-27 12:14:19 +0000

    cors update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL'), 'http://localhost:8182'],
+    'allowed_origins' => [env('FRONTEND_URL'), 'https://bytenblade.xyz'],
 
     'allowed_origins_patterns' => [],
 
